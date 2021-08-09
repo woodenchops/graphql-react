6197349f12c9bb3c41476e3ac8f806a10028feff
Kristian Woods <kris_woods@hotmail.co.uk>
2021-08-09 10:15:13 +0100

    add linting

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parser: '@javascript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@javascript-eslint', 'prettier'],
+  rules: {
+    'consistent-return': 'off',
+    'no-console': 'warn',
+    'no-use-before-define': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'import/no-unresolved': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'react/button-has-type': 'off',
+    'no-restricted-globals': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': 'off',
+    'import/prefer-default-export': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'no-underscore-dangle': 'off',
+    'no-param-reassign': 'off',
+  },
+};
